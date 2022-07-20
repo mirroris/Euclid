2ae7bfac1a8714e5565d2218b2751eed9dbe2499
mirroris <u455017k@ecs.osaka-u.ac.jp>
2022-07-20 23:02:25 +0900

    末永数列まで

--- Euclidean.c
+++ Euclidean.c
@@ -2,11 +2,15 @@
 
 long pyn=535225;
 long e=521;
-long a[15];
-long b[15];
-long c[15];
+long a[30];
+long b[30];
+long c1[30];
+long c2[30];
+
+#define MAX 29;
 
 int main(){
+    int N;
     long x,y,z;
     long count;
     b[0]=pyn;
@@ -23,9 +27,25 @@ int main(){
         b[i+2]=y;
 
         printf("%ld = %ld*%ld+%ld\n",b[i],a[i+1],b[i+1],b[i+2]);
-        if(b[i+2]==1)break;
+        if(b[i+2]==1){
+            N=i+2;
+            break;
+        }
+        if(i==MAX){
+            printf("something wrong!\n");
+            exit(1);
+        }
     }
 
-    
+    c1[0]=-a[N-1];
+    c2[0]=1;
+    printf("1 = ");
+    printf("%ld*%ld + %ld*%ld\n",c1[0],b[N-1],c2[0],b[N-2]);
+    for(int i=1;i<N;i++){
+        c1[i]=-c1[i-1]*a[N-i-1]+c2[i-1];
+        c2[i]=c1[i-1];
+        printf("%ld*%ld + %ld*%ld\n",c1[i],b[N-i-1],c2[i],b[N-i-2]);
+    }
+
     return 0;
 }
